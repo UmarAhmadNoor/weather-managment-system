4e664c9ed93d72d162a6370374c0ab62aa925289
Umar Ahmad <112937647+UmarAhmadNoor@users.noreply.github.com>
2024-07-24 12:45:42 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,43 +6,43 @@ const options = {
 	}
 };
 
-let getweather=(city)=>
-{
-  cityname.innerHTML=city
- fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+city, options)
- .then(response =>
-  
-    response.json())
-  
-    .then((response)=>
- {
-        // cloud_pct.innerHTML=response.cloud_pct
-        temp.innerHTML=response.temp
-        temp2.innerHTML=response.temp
-        feels_like.innerHTML=response.feels_like                      
-        humidityy.innerHTML= response.humidity 
-        humidity2.innerHTML= response.humidity 
-        min_temp.innerHTML= response.min_temp    
-        max_temp.innerHTML=response.max_temp   
-        wind_speed.innerHTML= response.wind_speed
-        wind_speed2.innerHTML= response.wind_speed
-        wind_degrees.innerHTML=response.wind_degrees 
-        sunrise.innerHTML= response.sunrise
-        sunset.innerHTML=response.sunset 
-        console.log(response)
-               
-      }
-     
- )
-    }
-    submit.addEventListener("click",(e)=>
-    {
-        e.preventDefault()
-        getweather(city.value)
-    })
-    getweather("Faisalabad")
-   
+const getweather = (city) => {
+    const cityname = document.getElementById('cityname');
+    const temp = document.getElementById('temp');
+    const feels_like = document.getElementById('feels_like');
+    const humidityy = document.getElementById('humidityy');
+    const min_temp = document.getElementById('min_temp');
+    const max_temp = document.getElementById('max_temp');
+    const wind_speed = document.getElementById('wind_speed');
+    const wind_degrees = document.getElementById('wind_degrees');
+    const sunrise = document.getElementById('sunrise');
+    const sunset = document.getElementById('sunset');
 
-    
+    cityname.innerHTML = city;
 
-    
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+        .then(response => response.json())
+        .then(response => {
+            temp.innerHTML = response.temp;
+            feels_like.innerHTML = response.feels_like;
+            humidityy.innerHTML = response.humidity;
+            min_temp.innerHTML = response.min_temp;
+            max_temp.innerHTML = response.max_temp;
+            wind_speed.innerHTML = response.wind_speed;
+            wind_degrees.innerHTML = response.wind_degrees;
+            sunrise.innerHTML = response.sunrise;
+            sunset.innerHTML = response.sunset;
+            console.log(response);
+        })
+        .catch(error => console.error('Error:', error));
+};
+
+const submit = document.getElementById('submit');
+const cityInput = document.getElementById('city');
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    getweather(cityInput.value);
+});
+
+getweather('Faisalabad');
